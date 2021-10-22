8154a0f4be7806d2a822cc2b2d0df499cc3c0df8
Gaeun Kim <gkim57@my.centennialcollege.ca>
2021-10-22 11:15:10 +0900

    Authentication with JWT

--- server/config/db.js
+++ server/config/db.js
@@ -8,5 +8,6 @@ Date: 2021.10.21
 module.exports = 
 {
     // "URI": "mongodb://127.0.0.1/business_contact"
-    "URI": "mongodb+srv://gaeun:ZinXJjktT2enlHRp@mongodbserver.ra1y4.mongodb.net/business_contact?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://gaeun:ZinXJjktT2enlHRp@mongodbserver.ra1y4.mongodb.net/business_contact?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'    
 }
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -10,6 +10,10 @@ let router = express.Router();
 let mongoose = require('mongoose');
 let passport = require('passport');
 
+// enable jwt
+let jwt = require('jsonwebtoken');
+let DB = require('../config/db');
+
 // create the User Model instance
 let userModel = require('../models/user');
 let User = userModel.User; // alias
@@ -71,6 +75,28 @@ module.exports.processLoginPage = (req, res, next) => {
             {
                 return next(err);
             }
+
+            const payload = 
+            {
+                id: user._id,
+                displayName: user.displayName,
+                username: user.username,
+                email: user.email
+            }
+
+            const authToken = jwt.sign(payload, DB.Secret, {
+                expiresIn: 604800 // 1 week
+            });
+
+            /* TODO - Getting Ready to convert to API
+            res.json({success: true, msg: 'User Logged in Successfully!', user: {
+                id: user._id,
+                displayName: user.displayName,
+                username: user.username,
+                email: user.email
+            }, token: authToken});
+            */
+
             return res.redirect('/contact-list');
         });
     })(req, res, next);
@@ -126,6 +152,10 @@ module.exports.processRegisterPage = (req, res, next) => {
             // if no error exists, then registeration is successful
 
             // redirect the user and authenticate them
+
+            /* TODO - Getting Ready to convert to API
+            res.json({success: true, msg: 'User Registered Successfully!'});
+            */
 
             return passport.authenticate('local')(req, res, () => {
                 res.redirect('/contact-list')
